1ae1ca10faa629a31a8565dfbb50a215a341b7cf
Simone Artini <95907355+simoneart@users.noreply.github.com>
2024-01-30 11:38:46 +0100

    Update fast.py
    Some debugging and I realized that I need to do degenerate perturbation theory.

--- fast.py
+++ fast.py
@@ -19,13 +19,18 @@ def generate_arrays(N):
 def parity_separator(sector_energies, all_energies, whole_basis): 
     E = [e for e in sector_energies]
     sector_basis = []
-    epsilon = 10**(-14) #must be appropriate, choose with care
+    '''Must be appropriate, choose with care: '''
+    epsilon = 10**(-13) 
+    '''This is not a reliable method.'''
     
     for j in range(2**N):
         for i in range(len(E)):
             if abs(E[i] - all_energies[j]) < epsilon:
                 sector_basis.append(whole_basis[j])
                 break
+    
+    if np.shape(sector_basis)[0] != 2**(N-1):
+        print("WARNING: the sector basis does not have the right dimension!!")
             
     '''This break guarantees the right order in the arrangement of the basis vectors while
     counting the degeneracies the right amount of times. In fact, in this way I associate one vector 
@@ -42,28 +47,30 @@ def even_excitation_energy(nk,J,g):
             energy += e(abs(K0[i]),J,g)
     return energy
 
-#energy correction as found analitically
+'''I NEED TO USE DEGENERATE PERTURBATION THEORY!!!!'''
+
+'''Given the vector written in the Fock basis, 
+this function gives the diagonal matrix element of V.'''
 def even_correction_term(nk,J,g,l): 
     corr = 0
-    K_sum = []
     for i in range(0,int(N/2)): #establishing the ks that give a contribution
-        if nk[i+int(N/2)] == 1 and  nk[-i+int(N/2)] == 1:
-            K_sum.append(K0[i+int(N/2)])
-    for k in K_sum:
-        corr += l/N*2*np.sin(2**theta(k,J,g))**2
-    if nk.all() == 0:
+        if nk[i+int(N/2)] == 1 and  nk[-i-1+int(N/2)] == 1:
+            corr += l/N*np.sin(2**theta(K0[i+int(N/2)],J,g))**2
+    if (nk == 0).all():
         corr = sum([l/N*np.sin(2**theta(k,J,g))**2 for k in K0p])
     return corr
 
 
-def basis_correction_term(basis,even_sector_energies,nks,nk_right,eright,J,g,l): 
-    corr_vector = np.zeros(2**N)
+'''Given the right vector written in the Fock basis,
+this function return an array containing the sandwiches
+between the rest of the basis and the given vector.'''
+'''I have checked the non-null terms with N=4. '''
+def od_matrix_elem(basis,nks,nk_right,J,g,l): 
     counter = 0
-    #I have to put the condition t
-    
-    for nk_left in nks: #sum over the basis
+    m_elem = []
+    for nk_left in nks: #sum over the basis of the sector
         corr = 0
-        if nk_left.all() != nk_right.all(): #only off-diagonal terms are non zero
+        if any(n != m for n,m in zip(nk_left,nk_right)): #only off-diagonal terms are non zero
             for i in range(int(N/2),N): #computing a single off-diagonal term, sums over the positive momenta
                 for j in range(int(N/2),N):    
                     if i == j: #check that if k=q the contribution is 0 (it should be granted by the other conditions, but it's faster this way)
@@ -77,7 +84,6 @@ def basis_correction_term(basis,even_sector_energies,nks,nk_right,eright,J,g,l):
                             s3 = (-1)**sum([nk_left[k] for k in range(0,i-int(N/2))])
                             s4 = (-1)**(sum([nk_left[k] for k in range(0,i)]) + 1)
                             corr += s1*s2*s3*s4*np.sin(2*theta(K0[i],J,g))*np.sin(2*theta(K0[j],J,g))
-                            
                         #second term
                         if nk_right[i] == 0 and nk_right[i-int(N/2)] == 0 and nk_right[j] == 0 and nk_right[j-int(N/2)] == 0 \
                             and nk_left[i] == 1 and nk_left[i-int(N/2)] == 1 and nk_left[j] == 1 and nk_left[j-int(N/2)] == 1:
@@ -103,20 +109,15 @@ def basis_correction_term(basis,even_sector_energies,nks,nk_right,eright,J,g,l):
                             s4 = (-1)**(sum([nk_left[k] for k in range(0,i)])) #excpet here
                             corr += -s1*s2*s3*s4*np.sin(2*theta(K0[i],J,g))*np.sin(2*theta(K0[j],J,g)) #extra -1
             
+        m_elem.append(-l/N*corr)
             
-            corr /= (eright - even_sector_energies[counter]) #actual correction factor
-            
-        corr_vector += corr * basis[counter] #building the correction to the basis vector
         counter += 1 #next term of the sum over the basis
-
-    return corr_vector
     
+    return np.array(m_elem)
+
     
 #------------------------------------------------------------------------------
-
-
 J, g, l = 1., 0.5, 0.1
-
 
 #complete spectrum and basis
 H0 = H_Ising(J, g)
@@ -124,19 +125,20 @@ eig_val0, eig_vect0 = eig(H0)
 eig_vec0 = np.array([eig_vect0[:,i] for i in range(len(eig_vect0[1]))])
 
 #EVEN sector basis and spectrum
-
-nks = np.array(generate_arrays(N))
+nks = np.array(generate_arrays(N)) #basis in the Fock representation, the order is important
 even_energies = np.array([even_excitation_energy(nk,J,g) for nk in nks])
+even_corrections = np.array([even_correction_term(nk,J,g,l) for nk in nks])
+even_perturbated_energies = even_energies + even_corrections
+
+'''
 #this is needed for the degenracy degree
 for i in range(2**(N-1)):
     if abs(even_energies[i]) < 10**(-14):
         even_energies[i] = 0.
-      
-even_corrections = np.array([even_correction_term(nk,J,g,l) for nk in nks])
-even_perturbated_energies = even_energies + even_corrections
+'''
+
 even_basis = parity_separator(even_energies,eig_val0,eig_vec0)
-even_perturbated_basis = [even_basis[i] + basis_correction_term(even_basis,even_energies,nks,nks[i],even_energies[i],J,g,l) \
-                          for i in range(2**(N-1))]
+even_matrix_elemets = [od_matrix_elem(even_basis,nks,nks[i],J,g,l) for i in range(2**(N-1))]
   
 
 
