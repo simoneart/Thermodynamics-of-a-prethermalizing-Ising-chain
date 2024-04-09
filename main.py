1b184b912b86a219f7476ca6254f6ebcf46f904f
Simone Artini <95907355+simoneart@users.noreply.github.com>
2024-04-09 15:44:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,73 @@
 import numpy as np
-from scipy.linalg import null_space
-from numpy.linalg import eig
 from Ising_chain_diagonalization import *
 from find_V_elements import *
-from perturbation_theory import *
-import itertools 
+from perturbation_theory import * 
+from testing_plateau import generate_initial_state, mode_pop
+import matplotlib.pyplot as plt
 import time
+from numba import jit
+
+
+'''
+I want to see the maximum number of sites I can consider without the perturbation.
+I want the spectrum, the basis and the time evolution of a mode.
+'''
+
 start_time = time.time()
 
-#------------------------------------------------------------------------------
-J, g, l = 1., 0.5, 0.01
+@jit
+def std_time_evo(Psi, e, t): 
+    '''
+    Parameters
+    ----------
+    Psi : coefficients of the expansion of the initial state Psi on the energy eigenbasis
+    e : energy spectrum 
+    
+    RK: they must be correctly ordered
 
-#EVEN sector basis and spectrum
-nks = np.array(generate_arrays(N)) #basis in the Fock representation, the order is important
+    Returns
+    -------
+    The time-evolved coefficients at time t of the state Psi expanded on the energy basis 
+    '''
+    
+    if len(e) != len(Psi):
+        raise ValueError("Both lists must have the same length")
+    
+    coeff_t = np.array([np.exp(-1j*e[i]*t)*Psi[i] for i in range(len(e))])
+    
+    return coeff_t
+
+
+J, g = 1., 0.5
+
+print("Basis ready")
+
+#Energy spectrum of the Hamiltonian
 even_energies = np.array([even_energies_excitation(nk,J,g) for nk in nks])
-#this is needed for the degenracy degree
-for i in range(2**(N-1)):
-    if abs(even_energies[i]) < 1e-14:
-        even_energies[i] = 0.
+        
+print("Spectrum ready")
+        
+#generate an initial state that is close to the GS
+Psi0 = generate_initial_state(2**(N-1))
+num_steps = 100
+dt = 0.1
 
-#matrix elements of V in the EVEN sector
-diag_elem = np.array([diag_V_elem(nk,J,g,l) for nk in nks])
-off_diag_elem = [od_V_elem(nks,nks[i],J,g,l) for i in range(2**(N-1))]
+nt = []
+taxis = []
 
+print("Initializing time evolution")
+#mode k = K0p[1]
+for k in range(num_steps): 
+    Psit = std_time_evo(Psi0, even_energies, k*dt)
+    nt.append(mode_pop(1, '+', Psit, nks))
+    taxis.append(k*dt)
 
-rep = find_repeating_indices(even_energies)
-degenerate_indices = list(rep.values()) 
-degenerate_energies = list(rep.keys())
+plt.figure(1)
+plt.plot(taxis, nt, '-.')
+plt.ylabel(r'$<n_k>$')
+plt.xlabel(r'$t$')
+plt.grid()
+plt.title('Time evolution of the population of the mode k=%1.3f' %K0p[1])
 
-#deg_space = degenerate_subspaces(even_energies, nks)
-
-V_ds = [V_subspace(diag_elem, off_diag_elem, ind) for ind in degenerate_indices]
-
-#Diagonalize them (RK: REMEMBER THAT EIG() RETURNS THE EIGENVECTORS AS COLUMNS)
-Vdiag_ds = [eig(V) for V in V_ds]
-fo_energies_corr = first_order_energy_corrections(degenerate_indices, diag_elem, Vdiag_ds)
-fo_even_energies = even_energies + fo_energies_corr
-
-so_energies_corr = second_order_energy_corrections(degenerate_indices, off_diag_elem, even_energies)
-so_even_energies = fo_even_energies + so_energies_corr
-
-#the degeneracy is not lifted
-rep1 = find_repeating_indices(so_even_energies) 
-degenerate_indices1 = list(rep1.values()) #for the third order I need these
-degenerate_energies1 = list(rep1.keys())
-
-'''Look into non-hermitianity (N=8 and 12, N=10 it doesn't happen')'''
-
-'''The potential breaks some simmetries of the system, but not all of them.
-Maybe this implies that the perturbation expansion will never break all the degeneracies
-sinche H + V is degenerate.'''
-
+print('Execution time:')
 print("--- %s seconds ---" % (time.time() - start_time))
